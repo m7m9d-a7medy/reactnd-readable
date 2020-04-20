1cf306d0d0112add415cf1edae1a2ea19c2188d1
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-20 05:23:31 +0200

    Basic routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 import { getPosts } from '../store/posts/actions'
 import { getCategories } from '../store/categories/actions'
-import Post from './components/Post/Post'
-import NewPost from './components/NewPost/NewPost'
-import EditPost from './components/EditPost/EditPost'
-import Comment from './components/Comment/Comment'
 import { State } from '../store/types'
 import { getComments } from '../store/comments/actions'
+import { Switch, Route, BrowserRouterProps } from 'react-router-dom'
+import Home from './routes/Home'
+import NewPost from './components/NewPost/NewPost'
+import EditPost from './components/EditPost/EditPost'
+import Post from './components/Post/Post'
 
 const mapStateToProps = (state: State, props: any) => {
   return {
@@ -32,47 +33,36 @@ const connector = connect(mapStateToProps, mapDispatchToProps)
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 
-type AppProps = PropsFromRedux & {}
+type AppProps = PropsFromRedux & BrowserRouterProps & {}
 
-class App extends Component<AppProps, {}> {
-  componentDidMount() {
-    this.props.getPosts()
-    this.props.getCategories()
-  }
+const App = (props: AppProps) => {
+  const { getCategories, getPosts } = props
 
-  state = {
-    postId: ''
-  }
+  useEffect(() => {
+    getPosts()
+    getCategories()
+  }, [getCategories, getPosts])
 
-  render() {
-    return (
-      <div>
-        {
-          this.props.postIds?.map(id => (
-            <div key={id} onClick={() => {
-              this.props.getComments(id)
-              this.setState({ postId: id })
-            }}>
-              <Post
-                id={id}
-              />
-            </div>
-          ))
-        }
-        <NewPost />
-        <EditPost id='' />
-        {
-          this.props.commentIds?.map(id => (
-            <Comment
-              key={id}
-              id={id}
-              parentId={this.state.postId}
-            />
-          ))
-        }
-      </div>
-    )
-  }
+  return (
+    <Switch>
+      <Route
+        exact path='/'
+        component={Home}
+      />
+      <Route
+        path='/posts/new'
+        component={NewPost}
+      />
+      <Route
+        path='/posts/edit/:id'
+        component={EditPost}
+      />
+      <Route
+        path='/posts/view/:id'
+        component={Post}
+      />
+    </Switch>
+  )
 }
 
 export default connector(App)
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './app/App'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './store'
 
 const app = (
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>
 )
 ReactDOM.render(app, document.getElementById('root'))
